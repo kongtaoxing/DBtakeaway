36bdac364f4b219a80797dcc2b23afcaacba99a4
kongtaoxing <xyf8658@163.com>
2023-04-22 00:04:49 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Table } from "react-bootstrap";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import './style/App.css';
 
 const App = () => {
